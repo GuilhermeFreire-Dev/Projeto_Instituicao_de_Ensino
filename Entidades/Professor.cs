364b6cc5160d82a27bfdfbd4b5d9fdb5693890e7
Guilherme Freire <guilhermefreire841@gmail.com>
2022-08-20 21:27:50 -0300

    Adicionando a classe Professor

---
+++ Entidades/Professor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instituicao_de_Ensino.Entidades
+{
+    class Professor
+    {
+    }
+}
